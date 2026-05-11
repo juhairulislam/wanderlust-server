17725bf08067413c2a873708292f05b52c270bb6
juhairulislam <juhairulislam2018@gmail.com>
2026-05-11 07:48:04 +0600

    add patch method

--- index.js
+++ index.js
@@ -62,6 +62,24 @@ async function run() {
 
 
 
+    app.patch("/destination/:id" , async (req, res) =>{
+
+      const {id} = req.params 
+      const updatedData = req.body 
+
+      const result = destinationCollection.updateOne(
+
+        {_id: new ObjectId(id)},
+        {$set: updatedData}
+
+      )
+
+      res.json(result)
+    })
+
+
+
+
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
